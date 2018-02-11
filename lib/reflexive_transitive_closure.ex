81e9c99af07d71129fbf318c61003cd98bfee76c
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-02-11 18:02:19 -0200

    Add graph adjacency map functions

--- lib/reflexive_transitive_closure.ex
+++ lib/reflexive_transitive_closure.ex
@@ -1,18 +1,18 @@
 defmodule ReflexiveTransitiveClosure do
-  @moduledoc """
-  Documentation for ReflexiveTransitiveClosure.
-  """
+  def add_edge(graph, edge) do
+    {source, destination} = edge
+    Map.get_and_update(graph, source, fn(edges) ->
+      {edges, edges && edges |> MapSet.put(destination) || MapSet.new([destination])}
+    end) |> elem(1)
+  end
 
-  @doc """
-  Hello world.
+  def merge_edges(graph1, graph2) do
+    Map.merge(graph1, graph2, fn _source, destinations_1, destinations_2 ->
+      MapSet.union(destinations_1, destinations_2)
+    end)
+  end
 
-  ## Examples
-
-      iex> ReflexiveTransitiveClosure.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def edge_list_to_adjacency_map(edge_list) do
+    Enum.reduce(edge_list, %{}, fn(x, acc) -> add_edge(acc, x) end)
   end
 end
--- test/reflexive_transitive_closure_test.exs
+++ test/reflexive_transitive_closure_test.exs
@@ -2,7 +2,20 @@ defmodule ReflexiveTransitiveClosureTest do
   use ExUnit.Case
   doctest ReflexiveTransitiveClosure
 
-  test "greets the world" do
-    assert ReflexiveTransitiveClosure.hello() == :world
+  test "adds edges to adjacency map" do
+    assert ReflexiveTransitiveClosure.add_edge(%{}, {1, 2}) == %{1 => MapSet.new([2])}
+    assert ReflexiveTransitiveClosure.add_edge(%{2 => MapSet.new([3])}, {2, 3}) == %{2 => MapSet.new([3])}
+    assert ReflexiveTransitiveClosure.add_edge(%{4 => MapSet.new([6])}, {4, 5}) == %{4 => MapSet.new([5, 6])}
+  end
+
+  test "merges adjacency maps" do
+    assert ReflexiveTransitiveClosure.merge_edges(
+      %{4 => MapSet.new([5, 6]), 5 => MapSet.new([6, 7])}, %{4 => MapSet.new([5, 8])}) ==
+      %{4 => MapSet.new([5, 6, 8]), 5 => MapSet.new([6, 7])}
+  end
+
+  test "converts edge list to adjacency map" do
+    assert ReflexiveTransitiveClosure.edge_list_to_adjacency_map([{0, 1}, {1, 2}, {1, 3}]) ==
+        %{0 => MapSet.new([1]), 1 => MapSet.new([2, 3])}
   end
 end
