1faf30b5f04127a408c8e340e2a4a1e242f0ee7c
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-02-11 20:01:28 -0200

    Add complex search test with graph loops

--- lib/reflexive_transitive_closure.ex
+++ lib/reflexive_transitive_closure.ex
@@ -30,7 +30,7 @@ defmodule ReflexiveTransitiveClosure do
 
   def dfs_from_vertex(adjacency_map, visited, source) do
     visited = visited |> MapSet.put(source)
-    reachable = MapSet.difference(adjacency_map.source, visited)
+    reachable = MapSet.difference(adjacency_map[source], visited)
     case Enum.fetch(reachable, 0) do
       {:ok, neighbor} ->
         visited = dfs_from_vertex(adjacency_map, visited, neighbor)
--- test/reflexive_transitive_closure_test.exs
+++ test/reflexive_transitive_closure_test.exs
@@ -3,9 +3,9 @@ defmodule ReflexiveTransitiveClosureTest do
   doctest ReflexiveTransitiveClosure
 
   test "adds edges to adjacency map" do
-    assert ReflexiveTransitiveClosure.add_edge(%{}, {1, 2}) == %{1 => MapSet.new([2])}
-    assert ReflexiveTransitiveClosure.add_edge(%{2 => MapSet.new([3])}, {2, 3}) == %{2 => MapSet.new([3])}
-    assert ReflexiveTransitiveClosure.add_edge(%{4 => MapSet.new([6])}, {4, 5}) == %{4 => MapSet.new([5, 6])}
+    assert ReflexiveTransitiveClosure.add_edge(%{}, {1, 2}) == %{1 => MapSet.new([2]), 2 => MapSet.new}
+    assert ReflexiveTransitiveClosure.add_edge(%{2 => MapSet.new([3])}, {2, 3}) == %{2 => MapSet.new([3]), 3 => MapSet.new}
+    assert ReflexiveTransitiveClosure.add_edge(%{4 => MapSet.new([6])}, {4, 5}) == %{4 => MapSet.new([5, 6]), 5 => MapSet.new}
   end
 
   test "merges adjacency maps" do
@@ -15,8 +15,10 @@ defmodule ReflexiveTransitiveClosureTest do
   end
 
   test "converts edge list to adjacency map" do
+    assert ReflexiveTransitiveClosure.edge_list_to_adjacency_map([{0, 1}, {1, 2}, {2, 1}]) ==
+        %{0 => MapSet.new([1]), 1 => MapSet.new([2]), 2 => MapSet.new([1])}
     assert ReflexiveTransitiveClosure.edge_list_to_adjacency_map([{0, 1}, {1, 2}, {1, 3}]) ==
-        %{0 => MapSet.new([1]), 1 => MapSet.new([2, 3])}
+        %{0 => MapSet.new([1]), 1 => MapSet.new([2, 3]), 2 => MapSet.new, 3 => MapSet.new}
   end
 
   test "does DFS" do
@@ -26,5 +28,24 @@ defmodule ReflexiveTransitiveClosureTest do
     assert ReflexiveTransitiveClosure.dfs(
       ReflexiveTransitiveClosure.edge_list_to_adjacency_map([{0, 1}, {1, 2}, {2, 0}])) ==
       ReflexiveTransitiveClosure.edge_list_to_adjacency_map([{0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2}, {2, 0}, {2, 1}, {2, 2}])
+    # 3───>2───>5───>0
+    # │    ^
+    # │    │
+    # │  ┌─┴───>6<─┐
+    # └─>1<─────┘  │
+    #    └──>7─────┘
+    # Input: [{1, 2}, {1, 6}, {1, 7}, {2, 5}, {3, 1}, {3, 2}, {5, 0}, {6, 1}, {7, 6}]
+    input = [{1, 2}, {1, 6}, {1, 7}, {2, 5}, {3, 1}, {3, 2}, {5, 0}, {6, 1}, {7, 6}]
+    output = ReflexiveTransitiveClosure.edge_list_to_adjacency_map([
+      {0, 0},
+      {1, 0}, {1, 1}, {1, 2}, {1, 5}, {1, 6}, {1, 7},
+      {2, 0}, {2, 2}, {2, 5},
+      {3, 0}, {3, 1}, {3, 2}, {3, 3}, {3, 5}, {3, 6}, {3, 7},
+      {5, 0}, {5, 5},
+      {6, 0}, {6, 1}, {6, 2}, {6, 5}, {6, 6}, {6, 7},
+      {7, 0}, {7, 1}, {7, 2}, {7, 5}, {7, 6}, {7, 7}])
+    assert ReflexiveTransitiveClosure.dfs(ReflexiveTransitiveClosure.edge_list_to_adjacency_map(input)) == output
+    # Test in random order
+    assert ReflexiveTransitiveClosure.dfs(ReflexiveTransitiveClosure.edge_list_to_adjacency_map(Enum.shuffle(input))) == output
   end
 end
